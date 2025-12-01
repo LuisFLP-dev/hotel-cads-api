683b677f502560f18e0501d2f6aed1093e8d9620
LuisFLP-dev <luislopes270403@gmail.com>
2025-12-01 00:43:01 -0300

    Criando codigo

--- src/main/java/br/ifs/edu/cads/api/hotel/entity/Estado.java
+++ src/main/java/br/ifs/edu/cads/api/hotel/entity/Estado.java
@@ -9,7 +9,7 @@ public class Estado {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(name = "uf", nullable = false, length = 255)
     private String uf;
@@ -36,5 +36,31 @@ public class Estado {
         this.uf = uf;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
 
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+
+        if (getClass() != obj.getClass())
+            return false;
+        Estado other = (Estado) obj;
+
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 }
